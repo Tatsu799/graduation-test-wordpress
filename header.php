b62188b153b9c000e4f2c80a3f6aa63364dbf6bb
Tatsu <nekonekokoneko788@gmail.com>
2023-12-30 20:03:54 +0900

    modify header.php

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?php bloginfo('name'); ?></title>
+    <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description"content="<?php bloginfo('description'); ?>">
     <meta name="keywords" content="web制作、ホームページ、コーポレートサイト制作">
